11be06586ee6a9f6a013ff75849f1c2410602359
Tiago <tiago.ameller@gmail.com>
2013-03-18 19:32:14 +0100

    version estable

--- src/com/example/pantallatactil/PantallaTactilActivity.java
+++ src/com/example/pantallatactil/PantallaTactilActivity.java
@@ -5,23 +5,13 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class PantallaTactilActivity extends Activity implements OnTouchListener {
 
 	private TextView tvAction;
-	private TextView tvPressure;
-	private TextView tvPressureMax;
-	private ProgressBar pbPressure;
-	private ImageView imPressureAlert;
-	private float mPressureMax = 0;
-	private TextView tvSize;
-	private TextView tvSizeMax;
-	private ProgressBar pbSize;
-	private ImageView imSizeAlert;
-	private float mSizeMax = 0;
+	private ProgressFragment frPressure;
+	private ProgressFragment frSize;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +21,24 @@ public class PantallaTactilActivity extends Activity implements OnTouchListener 
 		entrada.setOnTouchListener(this);
 
 		tvAction = (TextView) findViewById(R.id.tvAction);
-		tvPressure = (TextView) findViewById(R.id.tvPressure);
-		tvPressureMax = (TextView) findViewById(R.id.tvPressureMax);
-		imPressureAlert = (ImageView) findViewById(R.id.imPressureAlert);
-		pbPressure = (ProgressBar) findViewById(R.id.pbPressure);
-		tvSize = (TextView) findViewById(R.id.tvSize);
-		tvSizeMax = (TextView) findViewById(R.id.tvSizeMax);
-		pbSize = (ProgressBar) findViewById(R.id.pbSize);
-		imSizeAlert = (ImageView) findViewById(R.id.imSizeAlert);
+		frPressure = (ProgressFragment) getFragmentManager().findFragmentById(
+				R.id.frPressure);
+		frPressure.setValueName("Pressure");
+		frSize = (ProgressFragment) getFragmentManager().findFragmentById(
+				R.id.frSize);
+		frSize.setValueName("Size");
+	}
 
-		doReset(null);
+	public void doReset(View v) {
+		doShow(-1,0,0);
 	}
 
 	private void doShow(int aAction, float aPressure, float aSize) {
+		boolean bReset = false;
 		switch (aAction) {
 		case -1:
 			tvAction.setText("Not pressed yet");
+			bReset = true;
 			break;
 		case 0:
 			tvAction.setText("ACTION_DOWN");
@@ -58,67 +50,20 @@ public class PantallaTactilActivity extends Activity implements OnTouchListener 
 			tvAction.setText("ACTION_MOVE");
 			break;
 		}
-
-		if (aPressure > mPressureMax) {
-			mPressureMax = aPressure;
-		}
-		if (aAction < 0) {
-			tvPressureMax.setText("");
-		} else {
-			String s = Float.toString(mPressureMax);
-			if (s.length() > 4) {
-				s = s.substring(0, 4);
-			}
-			tvPressureMax.setText(s);
-		}
-		tvPressure.setText(Float.toString(aPressure));
-
-		if (aSize > mSizeMax) {
-			mSizeMax = aSize;
-		}
-		if (aAction < 0) {
-			tvSizeMax.setText("");
-		} else {
-			String s = Float.toString(mSizeMax);
-			if (s.length() > 4) {
-				s = s.substring(0, 4);
-			}
-			tvSizeMax.setText(s);
-		}
-		tvSize.setText(Float.toString(aSize));
-
-		doProgress(aPressure, aSize);
-
+		frPressure.UpdateValue(bReset,aPressure);
+		frSize.UpdateValue(bReset,aSize);
+		
 	}
-
-	public void doReset(View v) {
-		doShow(-1, 0, 0);
-	}
-
+	
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
-		doShow(event.getAction(), event.getPressure(), event.getSize());
 		if (event.getAction() == MotionEvent.ACTION_UP) {
-			doProgress(0, 0);
+			frPressure.UpdateProgress(0);
+			frSize.UpdateProgress(0);
+		} else {
+			doShow(event.getAction(),event.getPressure(),event.getSize());
 		}
 		return true;
 	}
 
-	private void doProgress(float aPressure, float aSize) {
-		int iPressure = Math.round(aPressure * 100);
-		pbPressure.setProgress(iPressure);
-		if (iPressure > 100) {
-			imPressureAlert.setVisibility(View.VISIBLE);
-		}else{
-			imPressureAlert.setVisibility(View.GONE);
-		}
-
-		int iSize = Math.round(aSize * 100);
-		pbSize.setProgress(iSize);
-		if (iSize > 100) {
-			imSizeAlert.setVisibility(View.VISIBLE);
-		}else{
-			imSizeAlert.setVisibility(View.GONE);
-		}
-	}
 }
---
+++ src/com/example/pantallatactil/ProgressFragment.java
@@ -0,0 +1,87 @@
+package com.example.pantallatactil;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class ProgressFragment extends Fragment {
+
+	private TextView tvValueName;
+	private TextView tvValue;
+	private TextView tvValueMax;
+	private ProgressBar pbValue;
+	private ImageView imAlert;
+	private float mValueMax = 0;
+	public static final boolean RESET = true;
+	private String valueName;
+
+	public String getValueName() {
+		return valueName;
+	}
+
+	public void setValueName(String aValue) {
+		valueName = aValue;
+		tvValueName.setText(aValue);
+	}
+
+	// Called once the Fragment has been created in order for it to
+	// create its user interface.
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Create, or inflate the Fragment's UI, and return it.
+		// If this Fragment has no UI then return null.
+		View view = inflater.inflate(R.layout.fragment_progress, container,
+				false);
+		tvValueName = (TextView) view.findViewById(R.id.tvValueName);
+		tvValue = (TextView) view.findViewById(R.id.tvValue);
+		tvValueMax = (TextView) view.findViewById(R.id.tvMax);
+		imAlert = (ImageView) view.findViewById(R.id.imAlert);
+		pbValue = (ProgressBar) view.findViewById(R.id.pbValue);
+		return view;
+	}
+
+	// Called at the start of the visible lifetime.
+	@Override
+	public void onStart() {
+		super.onStart();
+		UpdateValue(RESET, 0);
+	}
+
+	private String cutTo4Chars(Float aValue) {
+		String s = Float.toString(aValue);
+		if (s.length() > 4) {
+			s = s.substring(0, 4);
+		}
+		return s;
+	}
+
+	public void UpdateValue(boolean aReset, float aValue) {
+		if (aValue > mValueMax) {
+			mValueMax = aValue;
+		}
+		if (aReset) {
+			tvValueMax.setText("");
+		} else {
+			tvValueMax.setText(cutTo4Chars(mValueMax));
+		}
+		tvValue.setText(cutTo4Chars(aValue));
+
+		UpdateProgress(aValue);
+	}
+
+	public void UpdateProgress(float aValue) {
+		int iValue = Math.round(aValue * 100);
+		pbValue.setProgress(iValue);
+		if (iValue > 100) {
+			imAlert.setVisibility(View.VISIBLE);
+		} else {
+			imAlert.setVisibility(View.GONE);
+		}
+	}
+}
